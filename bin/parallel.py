2ec1585f4da58288816fc976e8b004ca4d37d454
hmml <hmml.code@gmail.com>
2014-02-12 12:01:48 +0100

    (parallel.py) ignore lines starting with #

--- bin/parallel.py
+++ bin/parallel.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
             count = int(sys.argv[1])
 
     print "Pool size:", count
-    commands = [s.strip() for s in sys.stdin.readlines()]
+    commands = [s.strip() for s in sys.stdin.readlines() if not s.startswith('#')]
     print multiprocessing.Pool(count).map(os.system, commands)
